f65ed37231da32269cb495e81a04924619b614c4
Pranav <pranavkulkarni1406@gmail.com>
2025-05-24 15:30:50 +0530

    routerchanges

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+const basename = "/rescuerelif";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
