b30008f5d27df579f0e5418f3d2055336c929640
Pranav <pranavkulkarni1406@gmail.com>
2025-05-24 15:21:29 +0530

    change

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/rescuerelif/', // <-- IMPORTANT
+    build: {
+    outDir: 'build', // change dist to build
+  },
   plugins: [react()],
 })
