d39a6317d684f3965bd139906097a6960563b8cc
Pranav <pranavkulkarni1406@gmail.com>
2025-05-24 15:44:03 +0530

    change

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-const basename = "/rescuerelif";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
